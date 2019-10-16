44c2a3fe179abfc5a868d1388c468036bcaf3b31
Jens Uenk <jens.uenk@gmail.com>
2019-10-16 22:50:00 +0200

    - PM1912-16: Als gebruiker wil ik een eigen profiel met alle posts die ik heb geplaatst - PM1912-2: Als gebruiker wil ik berichten kunnen plaatsen - Base index.html with nav and footer

---
+++ profile.php
@@ -0,0 +1,140 @@
+
+<!-- Profile -->
+<div class="profile">
+    <!-- Profile info -->
+    <div class="profile-info">
+        <div class="card" style="width: 18rem;">
+            <img src="img/dummy/profile-image.png" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Jens Uenk</h5>
+                    <small>Lorem ipsum dolor, sit amet consectetur adipisicing is dolor veniam suscipit officia cumque iste magnam?</small>
+                </p>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">Naam: Uenk</li>
+                <li class="list-group-item">Voornaam: Jens</li>
+                <li class="list-group-item">Telefoonnummer: 0392837236</li>
+            </ul>
+            <div class="card-body">
+                <a href="#" class="card-link">Edit</a>
+            </div>
+        </div>
+    </div>
+
+    <!-- Posts -->
+    <div class="profile-wall">
+        <!-- Posts | New post -->
+        <div class="card">
+            <div class="card-body">
+                <form>
+                    <div class="form-group">
+                        <label for="exampleFormControlTextarea1">New post:</label>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    </div>
+                </form>
+                <form>
+                    <div class="form-group">
+                        <input type="file" class="form-control-file" id="exampleFormControlFile1">
+                    </div>
+                    <button type="button" class="btn btn-outline-primary btn-sm">Post</button>
+                </form>
+            </div>
+        </div>
+        
+        <!-- Posts | Text post (dummy) -->
+        <div class="card">
+            <!-- Posts | Text post (user header) -->
+            <div class="card-header">
+                <div class="row">
+                    <div class="col-sm-1">
+                        <img class="profile-pic-mini" src="img/dummy/profile-image.png">
+                    </div>
+                    <div class="col-sm">
+                        Jens
+                        <p class="card-text">
+                            <small>5 minutes ago</small>
+                        </p>
+                    </div>
+                    <!-- Posts | Text post (edit dutton) -->
+                    <div class="col-sm1">
+                        <button type="button" class="btn btn-outline-primary btn-sm">...</button>
+                    </div>
+                </div>
+            </div>
+            <!-- Posts | Text post (post) -->
+            <div class="card-body">
+                <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores non mollitia repellat, soluta molestias quasi explic</p>
+                <button type="button" class="btn btn-primary btn-sm">Like</button>
+                <small>12 Likes</small>
+            </div>
+            <!-- Posts | Text post (command section) -->
+            <div class="card-footer">
+                <div class="form-group">
+                    
+                    <p>Comments: <small> (1 comment)</small></p>
+                    
+                    <!-- Posts | Text post (command) -->
+                    <div class="card-header">
+                        <div class="row">
+                            <div class="col-sm-1">
+                                <img class="profile-pic-mini-comment" src="img/dummy/profile-image.png">
+                            </div>
+                            Jens
+                        </div>
+                        <div class="card-body">
+                            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores non mollitia repellat, soluta molestias quasi explic</p>
+                            <button type="button" class="btn btn-primary btn-sm">Like</button>
+                            <small>1 Like</small>
+                        </div>
+                    </div>
+                    <!-- Posts | Text post (new command) -->
+                    <label for="exampleFormControlTextarea1">New comment:</label>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                </div>
+                <button type="button" class="btn btn-outline-primary btn-sm">Comment</button>
+            </div>
+        </div>
+
+
+
+        <!-- Posts | Foto post (dummy) -->
+        <div class="card">
+            <!-- Posts | Text post (user header) -->
+            <div class="card-header">
+                <div class="row">
+                    <div class="col-sm-1">
+                        <img class="profile-pic-mini" src="img/dummy/profile-image.png">
+                    </div>
+                    <div class="col-sm">
+                        Jens
+                        <p class="card-text">
+                            <small>20 minutes ago</small>
+                        </p>
+                    </div>
+                    <!-- Posts | Text post (edit dutton) -->
+                    <div class="col-sm1">
+                        <button type="button" class="btn btn-outline-primary btn-sm">...</button>
+                    </div>
+                </div>
+            </div>
+            <!-- Posts | Text post (post) -->
+            <div class="card-body">
+                <img src="img/dummy/banner.jpg" class="card-img-top" alt="...">
+                <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores non mollitia repellat, soluta molestias quasi explic</p>
+                <button type="button" class="btn btn-primary btn-sm">Like</button>
+                <small>7 Likes</small>
+            </div>
+            <!-- Posts | Text post (command section) -->
+            <div class="card-footer">
+                <div class="form-group">
+                    
+                    <p>Comments: <small> (0 comments)</small></p>
+                    <!-- Posts | Text post (new command) -->
+                    <label for="exampleFormControlTextarea1">New comment:</label>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                </div>
+                <button type="button" class="btn btn-outline-primary btn-sm">Comment</button>
+            </div>
+        </div>
+    </div>
+</div>
