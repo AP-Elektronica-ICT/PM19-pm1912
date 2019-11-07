8cc81fe7d1c91220a6e9092e4619fd2991b7f1bc
Anton Van Den Heuvel <antoton@gmail.com>
2019-11-07 15:49:19 +0100

    Session ID added

--- jslogin.php
+++ jslogin.php
@@ -2,7 +2,6 @@
 session_start();
 require_once('config.php');
 
-
 $username = $_POST['username'];
 $password = sha1($_POST['password']);
 
@@ -10,9 +9,16 @@ $sql = "SELECT * FROM accounts WHERE username = ? AND password = ? LIMIT 1";
 $stmtselect  = $db->prepare($sql);
 $result = $stmtselect->execute([$username, $password]);
 
+while($row = $stmtselect->fetch()) {
+    $user = $row["username"];
+    $pass = $row["password"];
+    $id = $row["id"];
+}
+
 if($result){
 	if($stmtselect->rowCount() > 0){
-		$_SESSION['userlogin'] = $username;
+        $_SESSION['userlogin'] = $result;
+		$_SESSION['id'] = $id;
 	}else{
 		echo 'There no user for that combo';		
 	}
