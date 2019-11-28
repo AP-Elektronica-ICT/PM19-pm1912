830f17468f2f4b14a3465405b98b72dd4c104bea
Jens Uenk <jens.uenk@gmail.com>
2019-11-28 10:22:33 +0100

    Added unit test for register

---
+++ tests/RegisterTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+use registrationclass;
+
+
+class RegisterTest extends TestCase
+{
+    private $calc;
+
+    public function __construct()
+    {
+        $this->calc = new registrationclass("BuildTest","BuildTest","BuildTest","BuildTest","BuildTest",0,"BuildTest","BuildTest",0,0);
+        parent::__construct();
+    }
+
+
+    public function testAdd()
+    {
+        $value = $this->calc->add();
+        $this->assertEquals($value, true);
+    }
+    
+    
+    public function testDelete()
+	{
+		$value = $this->calc->remove();
+        $this->assertEquals($value, true);
+	}
+}
