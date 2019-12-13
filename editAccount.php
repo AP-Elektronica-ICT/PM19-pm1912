bf63a4a751a1def36cd41c5a77752a717dd78193
Anton Van Den Heuvel <antoton@gmail.com>
2019-12-13 22:20:32 +0100

    for online functionality

--- editAccount.php
+++ editAccount.php
@@ -19,9 +19,39 @@ else
     $account_id = $id;
 }
 ?>
+<?php
+if(isset($_POST['but_upload'])) {
+		$name = $_FILES['file']['name'];
+		$target_dir = "img/upload/";
+        $target_file = $target_dir . basename($_FILES["file"]["name"]);
+    
+        // Select file type
+        $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+    
+        // Valid file extensions
+        $extensions_arr = array("jpg","jpeg","png","gif");
+    
+        // Check extension
+        if( in_array($imageFileType,$extensions_arr) ){
+        
+            // Insert record
+        $db->query("insert into images(ImageFileName, ImageOwner) values('".$name."', ".$id.")");
+		$last_querys = $db->query('SELECT * FROM images ORDER BY ImageID DESC LIMIT 1')->fetchAll();
+            foreach ($last_querys as $last_query) {
+                $last = $last_query;
+            }
+			// Upload file
+		move_uploaded_file($_FILES['file']['tmp_name'],$target_dir.$name);
+
+		$db->query("insert into profielfoto(userID, imageID) values(".$id.", ".$last['ImageId'].")");
+		echo "<script>window.location = 'index.php?account_id=".$_SESSION['id']."&page=profile'</script>";
+
+}
+}
+?>
 <div>
 	<div>Update profile-picture<div>
-	<form action = "updateProfPic.php" method = 'post' enctype='multipart/form-data'>
+	<form method = 'post' enctype='multipart/form-data'>
 						<input type='file' name='file' />
 						<input type='submit' value='Save name' name='but_upload'>
 	</form>
